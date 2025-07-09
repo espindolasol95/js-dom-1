97367c2b4ffa182ff07582fdab17f6358d2f1c4a
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-09 17:38:24 +0200

    feat (add commenti)

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const img = document.getElementById  ('lampadina')  //Recupero gli elementi HTML (bottone e immagine) tramite getElementById
+//Recupero gli elementi HTML (bottone e immagine) tramite getElementById
+//prendo l'immagine della lampadina usando il suo ID
+const img = document.getElementById  ('lampadina')  
+// stessa cosa con il bottone dalla pagina usando il suo ID
 const bottone = document.getElementById ('interrutore')
+
+
+
